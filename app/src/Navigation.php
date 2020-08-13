c563b5bfbb518babd66542adb0f005afe9853ab4
Jamie Peake <jamie.peake@gmail.com>
2020-08-14 02:43:43 +1000

    Late breaking addition of navigation

---
+++ app/src/Navigation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hive\Toy;
+
+/**
+ * Class Navigation
+ *
+ * Navigation sub system, allows an object to track it location on a board.
+ *
+ * In order to know oneself, one must understand where they are, what they are facing.
+ * (Board/Direction)
+ *
+ * @package Hive\Toy
+ */
+class Navigation
+{
+
+}
