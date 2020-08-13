d6a3736b6b40ef45aae99f5d07a0714bf13503f9
Jamie Peake <jamie@Notebook.lan>
2020-08-13 22:37:01 +1000

    Board Test Stubs

---
+++ app/tests/unit/BoardTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Hive\Toy\Test;
+
+class BoardTest extends \PHPUnit\Framework\TestCase
+{
+    // The default values
+    public function test_construct_withNullCoordinateArguments_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withValidCoordinateArguments_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withStringCoordinateArguments_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withFalseCoordinateArguments_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    // Zero the number
+    public function test_construct_withZeroCoordinateArguments_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withZeroXCoordinateArgument_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withZeroYCoordinateArgument_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withNegativeCoordinateArguments_isInvalid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withLargeCoordinateArguments_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withNullCoordinateArgumentsGetAxis_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withNullCoordinateArgumentsGetX_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withNullCoordinateArgumentsGetY_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withValidCoordinateArgumentsGetAxis_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withValidCoordinateArgumentsGetX_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withValidCoordinateArgumentsGetY_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withSetXGetX_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withSetYGetY_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withSetYGetAxis_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withSetXGetAxis_isValid()
+    {
+        $this->fail('soon');
+    }
+
+    public function test_construct_withSetXandYGetAxis_isValid()
+    {
+        $this->fail('soon');
+    }
+}
