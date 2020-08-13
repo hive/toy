15a27fbe0d95742f8ff041544b93755d30fb8ddd
Jamie Peake <jamie@Notebook.lan>
2020-08-13 21:34:41 +1000

    Initialise

---
+++ app/tests/unit/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+namespace Xplor\Test\Bootstrap;
+
+require('vendor/autoload.php');
+
+// Make php Notices ON for unittest
+error_reporting(E_ALL);
---
+++ app/tests/unit/SanityTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Xplor\Toy\Test;
+
+class SanityTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_sanity_isTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
